5a957d4250f6699d8c82b112efca9113a32679cb
YangWu1227 <yangwu2020@gmail.com>
2022-03-02 18:08:21 -0500

    feat: add custom exceptions for cleaning module

--- src/pycitizen/exceptions.py
+++ src/pycitizen/exceptions.py
@@ -15,3 +15,49 @@ class ColumnDtypeInferError(Exception):
 
     def __str__(self):
         return f'The dtypes of the following columns cannot be inferred: {self.col_nms}'
+
+# ---------------------------------------------------------------------------- #
+#                        Data cleaning module exceptions                       #
+# ---------------------------------------------------------------------------- #
+
+# ------------------------------- Keyword error ------------------------------ #
+
+
+class ColumnNameKeyWordError(Exception):
+    """
+    Exception raised when column names are keywords.
+    """
+
+    def __init__(self, col_nms):
+        self.col_nms = col_nms
+
+    def __str__(self):
+        return f'Columns {self.col_nms} are keywords of the language, and cannot be used as ordinary identifiers'
+
+# -------------------------- Start with digit error -------------------------- #
+
+
+class ColumnNameStartWithDigitError(Exception):
+    """
+    Exception raised when column names start with digits.
+    """
+
+    def __init__(self, col_nms):
+        self.col_nms = col_nms
+
+    def __str__(self):
+        return f'Columns {self.col_nms} must not start with digits'
+
+# ------------------------ Catch-all identifier error ------------------------ #
+
+
+class InvalidIdentifierError(Exception):
+    """
+    Exception raised when column names are invalid identifiers.
+    """
+
+    def __init__(self, col_nms):
+        self.col_nms = col_nms
+
+    def __str__(self):
+        return f'Columns {self.col_nms} are invalid identifiers'

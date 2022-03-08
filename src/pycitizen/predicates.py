423b1f35017cd264743db9650dc027495854e0d2
YangWu1227 <yangwu2020@gmail.com>
2022-03-08 01:00:42 -0500

    feat: add new validation function that includes str

--- src/pycitizen/predicates.py
+++ src/pycitizen/predicates.py
@@ -24,3 +24,20 @@ def is_sequence(seq):
         `True` if the sequence is a `collections.abc.Sequence` and not a string or bytestring.
     """
     return isinstance(seq, Sequence) and not isinstance(seq, (str, ByteString, range))
+
+
+def is_sequence_str(seq):
+    """
+    This helper returns `True` if the input is a `collections.abc.Sequence` (except bytestrings).
+
+    Parameters
+    ----------
+    seq : Sequence of objects
+        An input sequence to be tested.
+
+    Returns
+    -------
+    bool
+        `True` if the sequence is a `collections.abc.Sequence` and not a bytestring.
+    """
+    return isinstance(seq, Sequence) and not isinstance(seq, (ByteString, range))

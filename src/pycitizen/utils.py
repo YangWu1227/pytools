5f6cb648e83a4687afb5df9bf2c20b585274aa93
YangWu1227 <yangwu2020@gmail.com>
2022-05-12 11:49:15 -0400

    refactor: remove unused predicates from utils module

--- src/pycitizen/utils.py
+++ src/pycitizen/utils.py
@@ -34,23 +34,6 @@ def is_sequence(seq: Sequence) -> bool:
     return isinstance(seq, Sequence) and not isinstance(seq, (str, ByteString, range))
 
 
-def is_sequence_str(seq: Sequence) -> bool:
-    """
-    This helper returns `True` if the input is a `collections.abc.Sequence` (except bytestrings).
-
-    Parameters
-    ----------
-    seq : Sequence of objects
-        An input sequence to be tested.
-
-    Returns
-    -------
-    bool
-        `True` if the sequence is a `collections.abc.Sequence` and not a bytestring.
-    """
-    return isinstance(seq, Sequence) and not isinstance(seq, (ByteString, range))
-
-
 def is_list_str(seq: Sequence) -> bool:
     """
     This helper returns `True` if the input is either a list or a string.
